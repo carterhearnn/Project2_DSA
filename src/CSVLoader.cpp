62f8eef63ea4b0ef738242385bbdfbff08e9875d
acheng2007 <163091516+acheng2007@users.noreply.github.com>
2026-03-26 19:39:52 -0400

    csvloader function added for b+ tree construction

--- src/CSVLoader.cpp
+++ src/CSVLoader.cpp
@@ -44,6 +44,46 @@ std::map<std::string, RBTree> CSVLoader::loadCSV(const std::string& filepath) {
     return trees;
 }
 
+// Load CSV Functionality (1 B+ Tree per Ticker)
+std::map<std::string, BPlusTree> CSVLoader::loadCSV_BPlusTree(const std::string& filepath, int order) {
+    std::map<std::string, BPlusTree> trees;
+    std::ifstream file(filepath);
+
+    if (!file.is_open()) {
+        std::cerr << "Error: Could not open file " << filepath << std::endl;
+        return trees;
+    }
+
+    // Skip the header row
+    std::string line;
+    std::getline(file, line);
+
+    while (std::getline(file, line)) {
+        if (line.empty()) continue;
+
+        std::vector<std::string> fields = splitLine(line, ',');
+
+        // We have 8 fields: Date, Open, High, Low, Close, Volume, Brand_Name, Ticker
+        if (fields.size() < 8) continue;
+
+        int date = parseDate(fields[0]);
+        double open = std::stod(fields[1]);
+        double high = std::stod(fields[2]);
+        double low = std::stod(fields[3]);
+        double close = std::stod(fields[4]);
+        double volume = std::stod(fields[5]);
+        std::string ticker = fields[7];
+
+        Record record(date, open, high, low, close, volume);
+
+        auto [it, inserted] = trees.try_emplace(ticker, order);
+        it->second.insert(record);
+    }
+
+    file.close();
+    return trees;
+}
+
 // Remove dashes from date using parse function
 int CSVLoader::parseDate(const std::string& dateStr) {
     // Pull first 10 values of string
--- src/CSVLoader.h
+++ src/CSVLoader.h
@@ -2,6 +2,7 @@
 #define CSVLOADER_H
 
 #include "RBTree.h"
+#include "BPTree.h"
 #include <string>
 #include <map>
 #include <vector>
@@ -11,6 +12,9 @@ class CSVLoader {
 public:
     // Loads CSV and returns a <map> of ticker using an RBTree
     static std::map<std::string, RBTree> loadCSV(const std::string& filepath);
+
+    // Loads CSV and returns a <map> of ticker using a B+ Tree (can pick order)
+    static std::map<std::string, BPlusTree> loadCSV_BPlusTree(const std::string& filepath, int order);
 
 private:
     // Removes dashes in dates
